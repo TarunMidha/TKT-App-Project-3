0b0cf5dbdb2770d8fa46cc9f953fe58be966c16f
midhatarun <92733225+midhatarun@users.noreply.github.com>
2023-05-09 20:42:49 -0400

    V1

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TicketApp from "./components/TicketApp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TicketApp />
+    </div>
+  );
+}
+
+export default App;
